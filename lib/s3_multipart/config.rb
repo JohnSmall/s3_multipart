26f5c56a5a2491017b7994972fa289bb40240e65
John Small <jds340@gmail.com>
2014-05-25 07:52:54 +0000

    removed the check for the version number because we should be using semantic versioning and this method causes problems

--- lib/s3_multipart/config.rb
+++ lib/s3_multipart/config.rb
@@ -7,14 +7,7 @@ module S3Multipart
 
     def self.configure(block)
       block.call(self.instance)
-      check_for_breaking_changes
     end
 
-    def self.check_for_breaking_changes
-      version = S3Multipart::VERSION
-      if self.instance.revision != version
-        raise ArgumentError, "Breaking changes were made to the S3_Multipart gem:\n #{BREAKING_CHANGES[version.to_sym]}\n See the Readme for additional information."
-      end
-    end
   end
 end
